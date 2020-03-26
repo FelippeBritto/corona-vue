167afc0ac2dc79b8f3ce5168f5fdb9580164c545
Matheus Brito <mfelippe.britto@gmail.com>
2020-03-25 23:06:05 -0300

    First Commit

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Login">
+    <div class="logForm">
+      <img src="../assets/block.png" />
+      <h1>
+        <b>Administrador</b>
+      </h1>
+
+      <form>
+        <input type="password" placeholder="Senha do Administrador" />
+        <br />
+        <button type="submit" class="btn-login">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.btn-login {
+  background: rgb(33, 163, 44);
+  color: #fff;
+  height: 30px;
+  min-width: 17%;
+  border-block: none;
+}
+
+.logForm {
+  margin: auto;
+}
+</style>
